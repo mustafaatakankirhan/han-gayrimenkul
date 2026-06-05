d7cf2436eb072b88dc36cb379e6e6e8ef199b518
Mustafa Atakan <Hangayrimenkul@gmail.com>
2026-06-05 21:57:37 +0300

    feat: premium ilan detay sayfasi

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+ 
+const firebaseConfig = {
+  apiKey: "AIzaSyCS7MvN214pOaiVkvbEhwAqrK4CmA10kOg",
+  authDomain: "han-web-fe09d.firebaseapp.com",
+  projectId: "han-web-fe09d",
+  storageBucket: "han-web-fe09d.firebasestorage.app",
+  messagingSenderId: "860079702102",
+  appId: "1:860079702102:web:cce2e3fac0a8053ea1bbf7"
+};
+ 
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
